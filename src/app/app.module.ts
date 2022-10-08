e44def1ede155b9b05158a29d7a0924e57f31523
Christian Mulato <chmulato@hotmail.com>
2022-10-08 19:16:21 -0300

    Injeção de serviço no module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
+import { PokemonsService } from 'src/app/services/pokemons.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ListComponent } from './views/list/list.component';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    PokemonsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
